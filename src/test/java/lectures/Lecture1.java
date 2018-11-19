47dba4d9bbb811c9e9814d43d0a569017c35cfba
Mikita Simanavets <sema.nikita@gmail.com>
2018-11-19 20:12:16 +0100

    Add Imperative Approach Solution

--- src/test/java/lectures/Lecture1.java
+++ src/test/java/lectures/Lecture1.java
@@ -1,16 +1,11 @@
 package lectures;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import beans.Person;
-
 import com.google.common.collect.ImmutableList;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
 import mockdata.MockData;
-import org.assertj.core.util.Lists;
 import org.junit.Test;
 
 
@@ -19,9 +14,39 @@ public class Lecture1 {
   @Test
   public void imperativeApproach() throws IOException {
     List<Person> people = MockData.getPeople();
-    // 1. Find people aged less or equal 18
-    // 2. Then change implementation to find first 10 people
 
+    // 1. Find people aged less or equal 18
+    List<Person> youngPeople = new ArrayList<>();
+    for (Person person : people) {
+      if (person.getAge() <= 18) {
+        youngPeople.add(person);
+      }
+    }
+
+    for (Person young : youngPeople) {
+      System.out.println(young.getAge());
+    }
+
+    System.out.println("=====");
+
+    // 2. Then change implementation to find first 10 people
+    final int limit = 10;
+    int counter = 0;
+
+    List<Person> first10Below18 = new ArrayList<>();
+    for (Person person : people) {
+      if (person.getAge() <= 18) {
+        first10Below18.add(person);
+        counter++;
+        if (counter == limit) {
+          break;
+        }
+      }
+    }
+
+    for (Person young : first10Below18) {
+      System.out.println(young.getAge());
+    }
   }
 
   @Test
