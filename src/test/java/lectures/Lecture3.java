464e29f6f07b978f0b5054791b5cc3b8e57f6f96
Mikita Simanavets <sema.nikita@gmail.com>
2018-11-19 21:13:58 +0100

    Add finding min and max via Comparators

--- src/test/java/lectures/Lecture3.java
+++ src/test/java/lectures/Lecture3.java
@@ -1,6 +1,6 @@
 package lectures;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 import com.google.common.collect.ImmutableList;
 import java.util.Comparator;
@@ -10,14 +10,24 @@ import org.junit.Test;
 public class Lecture3 {
 
   @Test
-  public void min() throws Exception {
+  public void min() {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
 
+    Integer min = numbers.stream()
+        .min(Comparator.naturalOrder())
+        .get();
+
+    assertThat(min).isEqualTo(1);
   }
 
   @Test
-  public void max() throws Exception {
+  public void max() {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
 
+    Integer max = numbers.stream()
+        .max(Comparator.naturalOrder())
+        .get();
+
+    assertThat(max).isEqualTo(100);
   }
 }
