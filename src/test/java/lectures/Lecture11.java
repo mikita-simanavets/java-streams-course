d617f7d0f908d1ae92af0e942aa48d866463bf50
Mikita Simanavets <mikita.simanavets@pm.me>
2018-11-24 20:15:20 +0100

    Add joining Strings

--- src/test/java/lectures/Lecture11.java
+++ src/test/java/lectures/Lecture11.java
@@ -3,20 +3,32 @@ package lectures;
 import com.google.common.collect.ImmutableList;
 import java.util.List;
 import java.util.stream.Collectors;
-import mockdata.MockData;
 import org.junit.Test;
 
 public class Lecture11 {
 
   @Test
-  public void joiningStrings() throws Exception {
+  public void joiningStrings() {
     List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
 
+    StringBuilder result = new StringBuilder();
+    int lastNameIndex = names.size() - 1;
+    for (int i = 0; i < names.size(); i++) {
+      result.append(names.get(i))
+          .append(i < lastNameIndex ? ", " : "");
+    }
+
+    System.out.println(result);
   }
 
   @Test
-  public void joiningStringsWithStream() throws Exception {
+  public void joiningStringsWithStream() {
     List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
 
+    String result = names.stream()
+        .map(String::toUpperCase)
+        .collect(Collectors.joining("|"));
+
+    System.out.println(result);
   }
 }
