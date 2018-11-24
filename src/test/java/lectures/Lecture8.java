30db9427ef309b71bd1f43646d3f2fed30783af0
Mikita Simanavets <mikita.simanavets@pm.me>
2018-11-24 19:28:27 +0100

    Add grouping and counting data with streams like SQL

--- src/test/java/lectures/Lecture8.java
+++ src/test/java/lectures/Lecture8.java
@@ -1,12 +1,10 @@
 package lectures;
 
-
 import beans.Car;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import mockdata.MockData;
 import org.assertj.core.util.Lists;
@@ -16,11 +14,18 @@ public class Lecture8 {
 
   @Test
   public void simpleGrouping() throws Exception {
+    Map<String, List<Car>> carsByMake = MockData.getCars()
+        .stream()
+        .collect(Collectors.groupingBy(Car::getMake));
 
+    carsByMake.forEach((make, cars) -> {
+      System.out.println(make);
+      cars.forEach(System.out::println);
+    });
   }
 
   @Test
-  public void groupingAndCounting() throws Exception {
+  public void groupingAndCounting() {
     ArrayList<String> names = Lists
         .newArrayList(
             "John",
@@ -34,7 +39,9 @@ public class Lecture8 {
             "Alex"
         );
 
+    Map<String, Long> counting = names.stream()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
+    counting.forEach((name, count) -> System.out.println(name + " > " + count));
   }
-
 }
