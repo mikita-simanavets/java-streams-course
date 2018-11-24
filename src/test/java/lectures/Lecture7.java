6c06755f2ae53b435edd37278c55bce1f73a0ed3
Mikita Simanavets <mikita.simanavets@pm.me>
2018-11-24 18:59:33 +0100

    Add counting and statistics

--- src/test/java/lectures/Lecture7.java
+++ src/test/java/lectures/Lecture7.java
@@ -1,14 +1,11 @@
 package lectures;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import beans.Car;
-import com.google.common.collect.ImmutableList;
 import java.math.BigDecimal;
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
-import java.util.stream.Collectors;
 import mockdata.MockData;
 import org.junit.Test;
 
@@ -16,24 +13,48 @@ public class Lecture7 {
 
   @Test
   public void count() throws Exception {
+    long femaleCount = MockData.getPeople()
+        .stream()
+        .filter(person -> person.getGender().equalsIgnoreCase("female"))
+        .count();
 
+    assertThat(femaleCount).isEqualTo(467);
   }
 
   @Test
   public void min() throws Exception {
+    double minPrice = MockData.getCars()
+        .stream()
+        .filter(car -> car.getColor().equalsIgnoreCase("yellow"))
+        .mapToDouble(Car::getPrice)
+        .min()
+        .orElse(0);
 
+    assertThat(minPrice).isEqualTo(6600.59);
   }
 
   @Test
   public void max() throws Exception {
+    double maxPrice = MockData.getCars()
+        .stream()
+        .filter(car -> car.getColor().equalsIgnoreCase("yellow"))
+        .mapToDouble(Car::getPrice)
+        .max()
+        .orElse(0);
 
+    assertThat(maxPrice).isEqualTo(99677.14);
   }
 
 
   @Test
   public void average() throws Exception {
     List<Car> cars = MockData.getCars();
+    double averageCarPrice = cars.stream()
+        .mapToDouble(Car::getPrice)
+        .average()
+        .orElse(0);
 
+    assertThat(averageCarPrice).isEqualTo(52693.19979);
   }
 
   @Test
@@ -42,10 +63,12 @@ public class Lecture7 {
     double sum = cars.stream()
         .mapToDouble(Car::getPrice)
         .sum();
+
+    assertThat(sum).isEqualTo(5.269319979E7);
+
     BigDecimal bigDecimalSum = BigDecimal.valueOf(sum);
     System.out.println(sum);
     System.out.println(bigDecimalSum);
-
   }
 
   @Test
@@ -55,11 +78,11 @@ public class Lecture7 {
         .mapToDouble(Car::getPrice)
         .summaryStatistics();
     System.out.println(statistics);
-    System.out.println(statistics.getAverage());
-    System.out.println(statistics.getCount());
-    System.out.println(statistics.getMax());
-    System.out.println(statistics.getMin());
-    System.out.println(statistics.getSum());
-  }
 
+    assertThat(statistics.getAverage()).isEqualTo(52693.19979);
+    assertThat(statistics.getCount()).isEqualTo(1000);
+    assertThat(statistics.getMax()).isEqualTo(99932.82);
+    assertThat(statistics.getMin()).isEqualTo(5005.16);
+    assertThat(statistics.getSum()).isEqualTo(5.269319979E7);
+  }
 }
