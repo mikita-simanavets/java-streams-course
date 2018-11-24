5c36402a1e8f21f4bee26671da44442aad6b50d9
Mikita Simanavets <mikita.simanavets@pm.me>
2018-11-24 20:46:57 +0100

    Add Understanding section

--- src/test/java/lectures/Lecture12.java
+++ src/test/java/lectures/Lecture12.java
@@ -3,17 +3,21 @@ package lectures;
 import beans.Person;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import mockdata.MockData;
 import org.junit.Test;
 
 public class Lecture12 {
+
   @Test
   public void understandingCollect() throws Exception {
     List<String> emails = MockData.getPeople()
         .stream()
         .map(Person::getEmail)
-        .collect(Collectors.toList());
+        .collect(
+            ArrayList::new,
+            ArrayList::add,
+            ArrayList::addAll);
+//            Collectors.toList());
 
     emails.forEach(System.out::println);
   }
