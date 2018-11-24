62567f585540eb9c7554c679d9b46c6aa4c0c02b
Mikita Simanavets <mikita.simanavets@pm.me>
2018-11-24 19:38:05 +0100

    Add reduce

--- src/test/java/lectures/Lecture9.java
+++ src/test/java/lectures/Lecture9.java
@@ -1,18 +1,19 @@
 package lectures;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
-import com.google.common.collect.Lists;
 import java.util.Arrays;
 import org.junit.Test;
 
 public class Lecture9 {
 
   @Test
-  public void reduce() throws Exception {
+  public void reduce() {
     Integer[] integers = {1, 2, 3, 4, 99, 100, 121, 1302, 199};
 
+    Integer sum1 = Arrays.stream(integers).reduce(0, (a, b) -> a + b);
+    Integer sum2 = Arrays.stream(integers).reduce(0, Integer::sum);
+
+    assertThat(sum1).isEqualTo(sum2).isEqualTo(1831);
   }
-
-
 }
-
